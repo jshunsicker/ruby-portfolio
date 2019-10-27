24aa5992895a0535bda540fc9001a2b3f447592b
Jeremiah <sc.arletknight1807@gmail.com>
2019-10-27 13:17:33 -0400

    Implemented custom scopes for portfolio items. Added react items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,17 @@
 class Portfolio < ApplicationRecord
 
     validates_presence_of :title, :body, :main_image, :thumb_image
+
+    #Custom scopes for the model.
+    #So controller only managers data flow
+    #Instead, if you have something that should be relegated to the model
+    def self.react
+        where(subtitle: "React")
+    end
+
+    def self.rubyOnRails
+        where(subtitle: "Ruby on Rails")
+    end
+
+    scope :ruby_on_rails_portfolio_items, -> { where(subtitle: "Ruby on Rails")}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :portfolios, except: [:show]
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
+  get 'react-items', to: 'portfolios#react'
+
   resources :blogs do
     member do
       get :toggle_status
